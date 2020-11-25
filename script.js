9927b980036c56184d4dcfa3ea38c09efc346918
Kwis <random@gcom>
2020-11-25 22:45:04 +0800

    added html, css, js and updated some stuff

---
+++ script.js
@@ -0,0 +1,15 @@
+function getPassword(){
+
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
+    var passwordLenght = 20;
+    var password = "";
+
+    for (var i=0; i<passwordLenght; i++){
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber,randomNumber+1)
+    }
+
+
+    document.getElementById("password").value = password
+
+}
